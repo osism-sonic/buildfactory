fc9f6f5f35b31612dbc3451a384ad9844929853a
Christian Berendt <berendt@osism.tech>
2022-11-11 22:11:00 +0100

    Adjust configuration
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- build.sh
+++ build.sh
@@ -11,5 +11,17 @@ git submodule update --init
 cp -r ../overlays/x86_64-accton_as7726_32x-r0/* device/accton/x86_64-accton_as7726_32x-r0
 
 make init
-make configure PLATFORM=broadcom
-make SONIC_BUILD_JOBS=4 all
+
+# https://github.com/sonic-net/sonic-buildimage/blob/master/rules/config
+make configure \
+    PLATFORM=broadcom \
+    INCLUDE_ICCPD=y \
+    INCLUDE_KUBERNETES=y \
+    INCLUDE_RESTAPI=y \
+    SONIC_BUILD_JOBS=4 \
+    SONIC_CONFIG_USE_NATIVE_DOCKERD_FOR_BUILD=y \
+    SONIC_DPKG_CACHE_METHOD=rwcache \
+    SONIC_USE_DOCKER_BUILDKIT=y
+
+BLDENV=stretch make stretch
+make target/sonic-broadcom.bin
